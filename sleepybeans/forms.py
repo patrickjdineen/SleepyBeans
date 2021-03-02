c989e5460daf5272b0fca4ed100987d4c4143f67
Patrick Dineen <patrickjdineen@gmail.com>
2021-03-01 21:14:24 -0600

    Sign up and sign in done. add baby in progress

--- sleepybeans/forms.py
+++ sleepybeans/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DateField
 from wtforms.validators import DataRequired, Email
+from werkzeug.datastructures import MultiDict
+import datetime
 
 class UserLoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email() ])
     password = PasswordField('Password', validators= [DataRequired() ])
+    first = StringField('First Name')
+    last = StringField('Last Name')
+    name = StringField('Name')
+    birthdate = DateField('Date')
     submit_button = SubmitField()
--- sleepybeans/routes.py
+++ sleepybeans/routes.py
@@ -1,36 +1,43 @@
-from sleepybeans import app, db
+from sleepybeans import app, db, login_manager
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
 from sleepybeans.models import User, Baby, Sleep, check_password_hash
-from sleepybeans.forms import UserLoginForm
+from sleepybeans.forms import UserLoginForm, DateField
 
+#TBD
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(user_id)
+
+#Home
 @app.route('/')
 def home():
     return render_template('/home.html')
-
-
-
+#Sign Up Page
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
     form = UserLoginForm()
     if request.method == 'POST' and form.validate_on_submit():
         email = form.email.data
         password = form.password.data
-        user = User(email_address = email, password=password)
+        first = form.first.data
+        last = form.last.data
+        user = User(email_address = email, password=password, first_name = first,last_name=last)
         db.session.add(user)
         db.session.commit()
         return redirect (url_for('login'))
     return render_template('/signup.html', form=form)
 
+# Log In Page
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = UserLoginForm()
     print(form.data)
-    if request.method =='POST':
-        email=email.form.data
+    if request.method =='POST' and form.validate_on_submit():
+        email = form.email.data
         print(email)
-        password = password.form.data
+        password = form.password.data
         logged_user = User.query.filter(User.email_address == email).first()
         #NOTE FOR FUTURE. QUEURY.LAST COULD BE USED FOR MOST RECENT NAP???
         if logged_user and check_password_hash(logged_user.password,password):
@@ -38,10 +45,27 @@ def login():
             return redirect(url_for('profile'))
     return render_template('/login.html', form=form)
 
+#home profile page - TODO finish
 @app.route('/profile', methods = ['GET', 'POST'])
 def profile():
     return render_template('/profile.html')
 
+#Page to create baby and commit them to DB
+@app.route('/baby', methods= ['GET','POST'])
+def baby():
+    form = UserLoginForm()
+    if request.method == 'POST':
+        name = form.name.data
+        print(name)
+        new_baby = Baby(name=name)
+        print(new_baby)
+        db.session.add(new_baby)
+        db.session.commit()
+    else:
+        print("fail")
+    return render_template('/baby.html',form=form)
+
+#Sleep Tracking page
 @app.route('/sleep', methods= ['GET','POST'])
 def sleep():
     if request.form:
